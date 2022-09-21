7f9c9f57f228781297aebf75d686a1be7028c921
Qukka1401 <eremina.svetlana200@gmail.com>
2022-09-21 12:09:36 +0300

    New

--- Practice_tasks_1.py
+++ Practice_tasks_1.py
@@ -1,1 +1,19 @@
-print('Hi')
+import random
+
+count = 0
+lives = 3
+
+while lives>0:
+    n = random.randint(0, 10)
+    m = random.randint(0, 10)
+    s = n + m
+
+    an = int(input(f'Введите ответ: {n} + {m} = '))
+    if an == s:
+        print('Правильный ответ!')
+        count += 1
+    else:
+        print(f'Неправильный ответ! Жизней: {lives - 1}')
+        lives -= 1
+
+print(f'Игра окончена! Жизни закончились! Правильно решенных примеров {count}.')
