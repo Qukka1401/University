97f59aad45599274ed41665509fd0fca937df1f3
Qukka1401 <eremina.svetlana200@gmail.com>
2022-09-25 13:20:28 +0300

    New

--- Module_4/12.py
+++ Module_4/12.py
@@ -1,3 +1,12 @@
 a = int(input('Введите календарный месяц '))
 b = int(input('Введите день месяца '))
-if
+if b>=31:
+    a += 1
+    b = 1
+    print(f'{b}-{a}-2022')
+elif (a==2) and b>=28:
+    a += 1
+    b = 1
+    print(f'{b}-{a}-2022')
+else:
+    print(f'{b+1}-{a}-2022')
