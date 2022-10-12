be154b1f2eaf36968d830d323d226f68dfdc52be
Qukka1401 <eremina.svetlana200@gmail.com>
2022-10-12 12:18:33 +0300

    New

--- Practice_tasks_2.py
+++ Practice_tasks_2.py
@@ -1,2 +1,7 @@
+import random
 def p():
-    import random
+ for i in range (0,100):
+     a = [23,24,25,26,27,28,29,30,31,32] #Количество человек в классе
+     c = random.choice(a)
+     pc = c*(c-1)/2 #Подсчитывает количество пар
+
