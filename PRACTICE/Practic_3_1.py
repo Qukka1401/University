5b445ea67449b80b7ad319d9339099cdf009b9cb
Qukka1401 <eremina.svetlana200@gmail.com>
2022-10-26 12:25:51 +0300

    New

--- PRACTICE/Practic_3_1.py
+++ PRACTICE/Practic_3_1.py
@@ -1,20 +1,10 @@
 import Practic_3
 def create_list(*args, **kwargs):
-    r = []
-    for idx, val in enumerate(args):
-        r.append(f"Point_{idx} = {Practic_3.gms(val)}")
+    c = []
+    for i in range(len(args)):
+        c.append(f"Point_{i} = {Practic_3.gms(args[i])}")
+    for k, val in kwargs.items():
+        c.append(f"{k} = {Practic_3.gms(val)}")
+    return c
 
-    for k, v in kwargs.items():
-        r.append(f"{k} = {Practic_3.gms(v)}")
-    return r
-
-
-print(
-    create_list(
-        172.25899161,
-        321.42304971,
-        12.697987681352,
-        pole=21.89617856,
-        put_1=140.85706440,
-    )
-)
+print(create_list(12.9801, 35.1273, 162.0873, pole=15.3842, put=169.0521))
