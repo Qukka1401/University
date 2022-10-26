e48cc69813c6b5d4ed2e29cfea98fc4131c73e33
Qukka1401 <eremina.svetlana200@gmail.com>
2022-10-26 11:46:56 +0300

    New

--- Geom/__init__.py
+++ Geom/__init__.py
@@ -1,3 +1,3 @@
-from Geom.Practic_3 import * #инициализатор пакета с добавлением модулей для использования функций\ Имя пакета.Имя модуля
+from Geom.Gr_m_s import * #инициализатор пакета с добавлением модулей для использования функций\ Имя пакета.Имя модуля
 from Geom.Birthday import *
 from Geom.Montihall import *
