1a196f76d68b5f8acd5f105a63e222a06c5e2ee8
Qukka1401 <eremina.svetlana200@gmail.com>
2022-10-26 11:37:16 +0300

    New

--- Practice_Task/Practice_tasks_2.py
+++ Practice_Task/Practice_tasks_2.py
@@ -1,28 +1,16 @@
 import random
 
-days = [day for day in range(0, 29)]
-months = [month for month in range(0, 13)]
+def birthday(interp, people_count=23):
+    count = 0
+    for i in range(interp):
+        c = []
+        for j in range(people_count):
+            age = random.randint(1,365)
+            if age in c:
+                count += 1
+                break
+            c.append(age)
+    return count/(interp/100)
 
-def birthday(people_count):
-    D_data = []
-    M_data = []
+print(birthday(100000, 60))
 
-    count_coincidences = 0
-    count_not_coincidences = 0
-
-    for i in range(0, people_count + 1):
-        D_data.append(random.choice(days))
-        M_data.append(random.choice(months))
-
-    for i in range(0, people_count):
-        if D_data.count(D_data[i]) == 1 or M_data.count(M_data[i]) == 1:
-            count_not_coincidences += 1
-            pass
-
-        for j in range(0, people_count):
-            if j == i:
-                pass
-
-            if D_data[j] == D_data[i] and M_data[j] == M_data[i]:
-                count_coincidences += 1
-print(birthday())
