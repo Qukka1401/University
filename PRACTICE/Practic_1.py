f378072fddd104f7f01e461fe107a1fbf75038ef
Qukka1401 <eremina.svetlana200@gmail.com>
2022-11-08 16:38:29 +0300

    New

--- PRACTICE/Practic_1.py
+++ PRACTICE/Practic_1.py
@@ -1,17 +1,16 @@
 import random
-def montihall(inter):
-    vin1 = 0
-    vin2 = 0
-    for i in range (inter):
-        a = [0,1,0]
-        o = random.choice(a)
-        a.remove(o)
-        a.remove(0)
-        if o == 1:
-            vin1 +=1
-        elif a[0] == 1:
-            vin2 += 1
 
-    return (f'Процент выигрыша при первоначальном выборе: {vin1//100}, Процент выигрыша при изменении выбора {vin2//100}')
-
-
+count = 0
+lives = 3
+while lives>0:
+    n = random.randint(-10, 10)
+    m = random.randint(-10, 10)
+    s = n + m
+    an = int(input(f'Введите ответ: {n} + {m} = '))
+    if an == s:
+        print('Правильный ответ!')
+        count += 1
+    else:
+        print(f'Неправильный ответ! Жизней: {lives - 1}')
+        lives -= 1
+    print(f'Игра окончена! Жизни закончились! Правильно решенных примеров {count}.')
---
+++ Practice_tasks/Practice_tasks_1.py
@@ -0,0 +1,16 @@
+import random
+def montihall(inter):
+    vin1 = 0
+    vin2 = 0
+    for i in range (inter):
+        a = [0,1,0]
+        o = random.choice(a)
+        a.remove(o)
+        a.remove(0)
+        if o == 1:
+            vin1 +=1
+        elif a[0] == 1:
+            vin2 += 1
+
+    return (f'Процент выигрыша при первоначальном выборе: {vin1/100}, Процент выигрыша при изменении выбора {vin2/100}')
+
