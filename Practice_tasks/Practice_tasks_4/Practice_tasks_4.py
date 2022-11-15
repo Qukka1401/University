f86b61712a0853385f3829d71cced2142975e49a
Qukka1401 <eremina.svetlana200@gmail.com>
2022-11-15 19:57:26 +0300

    New

--- Practice_tasks/Practice_tasks_4/Practice_tasks_4.py
+++ Practice_tasks/Practice_tasks_4/Practice_tasks_4.py
@@ -1,10 +1,17 @@
+def read_file(filename):
+    ss = {}
+    with open(filename, encoding='UTF-8', mode='r') as f:
+        s = (f.read().split(' '))
+    for i in range(0, len(s)):
+        s[i] = s[i].lower()
+        s[i] = "".join(j for j in s[i] if j.isalpha())
+    ss = set(s)
+    return list(ss)
 
-def read_file():
-    word_list = open('data.txt', mode='r', encoding= 'utf-8')
-    s = word_list.read().lower()
-    a = 'йцукенгшщзхъфывапролджэячсмитьбюё'
-    for i in range(len(s)):
-        if not (s[i] in a):
-            s = s.replace(s[i], ' ')
-    return s
-print(read_file())
+def save_file(filename, s):
+    s = sorted(s)
+    with open(filename, encoding='utf-8', mode='w') as f:
+        f.write(f'Количество уникальных слов: ' + str(len(s)))
+        f.write("\n=====================\n")
+        f.write('\n'.join(s))
+
---
+++ Practice_tasks/Practice_tasks_4/test.py
@@ -0,0 +1,4 @@
+import Practice_tasks_4
+
+print(Practice_tasks_4.read_file('data.txt'))
+Practice_tasks_4.save_file('count.txt', Practice_tasks_4.read_file('data.txt'))
