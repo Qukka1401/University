66242c4df5a8aee5b67a8805002ee7b37d969fdb
Qukka1401 <eremina.svetlana200@gmail.com>
2022-11-16 10:51:08 +0300

    New

--- Practice_tasks/Practice_tasks_4/Practice_tasks_4.py
+++ Practice_tasks/Practice_tasks_4/Practice_tasks_4.py
@@ -1,10 +1,10 @@
 def read_file(filename):
     ss = {}
     with open(filename, encoding='UTF-8', mode='r') as f:
-        s = (f.read().split(' '))
+        s = (f.read().split(' ')) #split разделяет строки на список строк
     for i in range(0, len(s)):
         s[i] = s[i].lower()
-        s[i] = "".join(j for j in s[i] if j.isalpha())
+        s[i] = "".join(j for j in s[i] if j.isalpha()) #join возвращает строку, объединяя все элементы строки, разделенные разделителем строк; isalpha проверят строки на содержание элементов алфавита
     ss = set(s)
     return list(ss)
 
