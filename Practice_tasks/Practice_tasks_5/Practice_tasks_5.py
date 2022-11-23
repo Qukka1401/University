494d08b032a330f24c4fc779ac539ec60e4706cb
Qukka1401 <eremina.svetlana200@gmail.com>
2022-11-23 11:43:31 +0300

    New

--- Practice_tasks/Practice_tasks_5/Practice_tasks_5.py
+++ Practice_tasks/Practice_tasks_5/Practice_tasks_5.py
@@ -1,13 +1,25 @@
 def s(filename):
     try:
-        f = open(filename)
-        chisla = int(f.readline())
-        return [int(a) for a in [f.readline() for _ in range(chisla)]]
-    except ValueError:
+        f = open(filename, encoding='utf-8')
+        chisla = f.read().splitlines()
+        chisla = [int(a) for a in chisla]
+
+        if chisla[0] != len(chisla[1:]):
+            raise KeyError
+        return chisla[1:]
+
+    except KeyError:
+        return 'Количество чисел не соответсвует первому числу'
+    except ValueError: #Неправильный тип
         return 'Неверные значения'
-    except FileNotFoundError:
+    except FileNotFoundError: #Файла не существует, ошибка в имени
         return 'Неправильное имя файла'
+    except IndexError:
+        return 'Количество чисел неправильное'
     except:
         return 'Неизвестная ошибка'
+    finally:
+        f.close()
+
 filename = input('Введите имя файла: ')
-s(filename)
+print(s(filename))
