d02a929ac0a171612c5c6269e28b1e7a989a55c3
Qukka1401 <eremina.svetlana200@gmail.com>
2022-12-20 17:47:46 +0300

    New

--- PRACTICE/Practice_tasks/def_Practice_tasks_3/main.py
+++
@@ -1,9 +0,0 @@
-import rounds as s
-def start() -> None:
-    while True:
-        command = int(input('1 - Начать игру, 2 - Выход \n'))
-        if command == 1:
-            s.start_game()
-            print('Хотите сыграть еще?')
-        elif command == 2:
-            break
